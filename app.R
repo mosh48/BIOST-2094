1a66c907530ff9c37ae3ef288874ca3a3dd16eb9
Nick Moshgat <nmoshgat1@gmail.com>
2023-04-05 14:59:52 -0400

    added basic forms of sweetness slider, alc % slider, flavor boxes, and submit button

--- app.R
+++ app.R
@@ -16,8 +16,34 @@ sidebarLayout(
   sidebarPanel(
     #alcohol type input
     checkboxGroupInput(inputId="alcohol",
-                       label="Choose your alcohol preference(s)",
-                       choices=c("Vodka", "Gin", "Rum", "Tequila", "Whiskey", "No Preference"))
+                       label="Choose your alcohol preference(s):",
+                       choices=c("Vodka", "Gin", "Rum", "Tequila", "Whiskey", "No Preference")),
+    
+    #sweetness slider
+    sliderInput(inputId = "Sweetness",
+                label = "Choose your preferred level of sweetness:",
+                min = 1,
+                max = 5,
+                value = 3),
+    
+    #alcohol content slider
+    sliderInput(inputId = "ABV (%)",
+                label = "Choose your preferred alcohol content:",
+                min = 9,
+                max = 45,
+                value = 20), # might want to do intervals of 5 or something
+    
+    #flavors input
+    checkboxGroupInput(inputId = "Flavors",
+                       label = "Choose any flavors you would prefer:",
+                       choices = unique(unlist(strsplit(data$Flavors, ", ")))),
+                      #had to remove "," in column for each flavor
+                      #list might be a bit long
+      #maybe we could eliminate choices if there is not a drink that fits flavors
+    
+    #submit button that looks like martini glass
+    submitButton(text = "Find Drinks", icon = icon(name = "martini-glass-citrus"))
+    
   ),
   #main panel for displaying outputs
   mainPanel(
