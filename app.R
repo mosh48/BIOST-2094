0d096e4bb7752cdee92695119fb53cf862a9424f
Emma Lipinski <emmalipinski54@gmail.com>
2023-04-05 11:42:52 -0400

    Initial alcohol checkbox progress

--- app.R
+++ app.R
@@ -1,8 +1,35 @@
+#load shiny and readxl packages
 library(shiny)
+library(readxl)
 
-ui <- fluidPage()
+#load data
+data <- read_excel("ProjectData.xlsx")
 
+#define UI
+ui <- fluidPage(
+#app title
+titlePanel("Drink Finder: A Personalized Cocktail Recommender"),
+
+#input definitions
+sidebarLayout(
+  
+  sidebarPanel(
+    #alcohol type input
+    checkboxGroupInput(inputId="alcohol",
+                       label="Choose your alcohol preference(s)",
+                       choices=c("Vodka", "Gin", "Rum", "Tequila", "Whiskey", "No Preference"))
+  ),
+  #main panel for displaying outputs
+  mainPanel(
+    
+  )
+)
+)
+
+#define server logic
 server <- function(input, output) {
+  
 }
 
+#Run the app
 shinyApp(ui, server)
