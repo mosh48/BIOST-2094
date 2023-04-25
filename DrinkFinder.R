acbcfa636a10732776b26e9e9dc3bc37fa90c0e1
Emma Lipinski <emmalipinski54@gmail.com>
2023-04-25 10:55:50 -0400

    Finalizing files and deleting extra ones

--- DrinkFinder.R
+++ DrinkFinder.R
@@ -1,14 +1,25 @@
-#load appropriate packages
+#BIOST 2094 Final Project
+#Drink Finder
+#Li Fan, Emma Lipinski, Nick Moshgat, Nuo Wei
+
+#load packages
 library(shiny)
 library(readxl)
 library(tidyverse)
-library(jpeg)
 library(DT)
 
-#load data
+#set working directory
+#setwd("E:/University of Pittsburgh/BIOST 2094 Advanced R Computing/Project/")
+
+#load data and add row numbers
 data <- read_excel("ProjectData.xlsx")
-data$image <- rownames(data)
-Flavors <- unique(unlist(strsplit(data$Flavors, ", ")))
+data <- data %>% mutate(row_number = row_number())
+
+
+#load flavors options
+Flavors_options <- unlist(strsplit(data$Flavors, ", "))
+Flavors_options <- c("No Preference" , Flavors_options)
+
 
 #define UI
 ui <- fluidPage(
@@ -22,11 +33,10 @@ ui <- fluidPage(
       #alcohol type input
       checkboxGroupInput(inputId="Alcohol",
                          label="Choose your alcohol preference(s). If you have no preference, check all boxes:",
-                         choices=c("Vodka", "Gin", "Rum", "Tequila", "Whiskey")),
-      #could add unique(data$Alcohol)
+                         choices=unique(data$Alcohol)),
       
       #sweetness button
-      actionButton(inputId = "SweetnessButton", label = "Select a sweetness Level"),
+      actionButton(inputId = "SweetnessButton", label = "Next: Select a Sweetness Level"),
       
       #sweetness slider
       uiOutput("SweetnessSlider"),
@@ -46,59 +56,60 @@ ui <- fluidPage(
       
       DT::dataTableOutput("Table"),
       uiOutput("Images")
+      
     )
   )
 )
 
-# Define server function
+#define server logic
 server <- function(input, output) {
-
   
   # Present sweetness slider when the SweetnessButton is clicked
   observeEvent(input$SweetnessButton, {
     
     # Filter data based on selected alcohol
-    filtered_data <- data
     if (!is.null(input$Alcohol)) {
-      filtered_data <- filtered_data %>% filter(Alcohol %in% input$Alcohol)
+      data <- data %>% filter(Alcohol %in% input$Alcohol)
     }
     
     # Update sweetness slider
     output$SweetnessSlider <- renderUI({
       sliderInput(inputId = "SweetnessSlider",
                   label = "Choose your preferred level of sweetness:",
-                  min = min(filtered_data$Sweetness),
-                  max = max(filtered_data$Sweetness),
-                  value = round(mean(range(filtered_data$Sweetness))),
+                  min = min(data$Sweetness),
+                  max = max(data$Sweetness),
+                  value = round(mean(range(data$Sweetness))),
                   step = 1)
     })
     
     # present flavors button
     output$FlavorsButton <- renderUI({
-      actionButton(inputId = "FlavorsButton", label = "Select a flavor")
+      actionButton(inputId = "FlavorsButton", label = "Next: Select Flavors")
     })
+    
+    
   })
+  
   
   # Update available flavors when flavors button is chosen
   observeEvent(input$FlavorsButton, {
     
     # Filter available flavors based on selected alcohol and sweetness
-    filtered_data <- data
     if (!is.null(input$Alcohol)) {
-      filtered_data <- filtered_data %>% filter(Alcohol %in% input$Alcohol)
+      data <- data %>% filter(Alcohol %in% input$Alcohol)
     }
     
     # Sweetness + or - 1
     if (!is.null(input$SweetnessSlider)) {
-      filtered_data <- filtered_data[filtered_data$Sweetness >= (input$SweetnessSlider - 1) & filtered_data$Sweetness <= (input$SweetnessSlider + 1),]
+      data <- data[data$Sweetness >= (input$SweetnessSlider - 1) & data$Sweetness <= (input$SweetnessSlider + 1),]
     }
     
     # Get list of available flavors based on filtered data
-    flavors <- unique(unlist(strsplit(filtered_data$Flavors, ", ")))
+    flavors <- unique(unlist(strsplit(data$Flavors, ", ")))
     
     output$FlavorsCheckbox <- renderUI({
-      checkboxGroupInput("FlavorsCheckbox", "Choose any flavors you would prefer:", choices = flavors)
-    }) # maybe do drop down menu
+      checkboxGroupInput("FlavorsCheckbox", "Choose flavors that you prefer:", choices = flavors)
+    }) 
     
     # present submit button
     output$SubmitButton <- renderUI({
@@ -109,17 +120,13 @@ server <- function(input, output) {
   
   drink_filter <- reactive({
     
-    #Adds column of image path to remaining drinks
-    #data$image <- paste0("www/", data$Name, ".jpg")
-    
     # Filter cocktails based on user preferences
-    filtered <- data
     if (!is.null(input$Alcohol)) {
-      filtered <- filtered[filtered$Alcohol %in% input$Alcohol,]
+      data <- data[data$Alcohol %in% input$Alcohol,]
     }
     
     if (!is.null(input$SweetnessSlider)) {
-      filtered <- filtered[filtered$Sweetness >= (input$SweetnessSlider - 1) & filtered$Sweetness <= (input$SweetnessSlider + 1),]
+      data <- data[data$Sweetness >= (input$SweetnessSlider - 1) & data$Sweetness <= (input$SweetnessSlider + 1),]
     }
     
     if (!is.null(input$FlavorsCheckbox)) {
@@ -127,60 +134,59 @@ server <- function(input, output) {
       selected_flavors <- input$FlavorsCheckbox
       
       # split comma-separated flavors into separate strings
-      split_flavors <- str_split(filtered$Flavors, ",\\s*")
+      split_flavors <- str_split(data$Flavors, ",\\s*")
       
       # check if any of the selected flavors are present in each row
       selected_rows <- sapply(split_flavors, function(flavors) any(flavors %in% selected_flavors))
       
-      # filter dataset to include selected rows
-      filtered_data <- filtered[selected_rows, ]
-      filtered_data <- filtered_data #%>% select(-image)
+      # filter dataset to exclude selected rows
+      data <- data[selected_rows, ]
     }
-    
-    #Adds column of image path to remaining drinks
-    #filtered$image <- paste0("www/", filtered$Name, ".jpg")
-    
-    #filtered <- data[data$image != "", ]
-    filtered_data
-    
+    data
   })
   
   # output table when submit button clicked
   observeEvent(input$SubmitButton, {
     final_data <- drink_filter()
-    table <- final_data %>% select(-image)
-    output$Table <- DT::renderDataTable({
+    table <- final_data %>% select(-row_number)
+    output$Table <- renderDataTable({
       table
     })
-    
-    output$Images <- renderUI({
-      image_output_list <- lapply(final_data$image, function(i) {
-        imagename <- paste0(i, ".jpg")
-        imageOutput(imagename)
-      })
-      
-      do.call(tagList, image_output_list)
   })
   
-    
-    for (i in final_data$image){
+  
+  
+  observeEvent(input$SubmitButton, {
+    for (i in drink_filter()$row_number){
       local({
-        j <- i
-        Images = paste0(j, ".jpg")
-        #print(Images)
+        
+        Images = paste0(i, ".jpg")
         
         output[[Images]] <- renderImage({
           
-          list(src = file.path('wwww', Images), 
+          list(src = file.path('www', Images), 
                width=500, height=300)
         }, deleteFile=FALSE)
         
       })
+    }
+    
+    
+  })
   
-}
+  output$Images <- renderUI({
+    
+    image_output_list <- 
+      lapply(drink_filter()$row_number,
+             function(i){
+               imagename = paste0(i, ".jpg")
+               imageOutput(imagename)
+             })
+    
+    do.call(tagList, image_output_list)
   })
 }
 
-# Run the app
+
 shinyApp(ui = ui, server = server)
 
